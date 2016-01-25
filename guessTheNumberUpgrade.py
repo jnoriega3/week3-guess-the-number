373c2a05a447a91f20f69660327d3d66aa2849d2
Jessica Noriega <jnoriega3@mail.valenciacollege.edu>
2016-01-24 22:36:01 -0500

    Revert "Update guessTheNumberUpgrade.py"
    This reverts commit 8a7895ae0a756a50e0d712950ad895eb99f1d68f.

--- guessTheNumberUpgrade.py
+++ guessTheNumberUpgrade.py
@@ -52,7 +52,7 @@ def evaluateAnswer( userGuess, userSecretNumber ):
         print('Your guess is too low.')
         return False
         elif userGuess > userSecretNumber:
-        print('Your guess is too high.')
+        print('Your guess is too hight.')
         return False
         else:
         return True
